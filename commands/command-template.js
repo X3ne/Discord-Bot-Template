9bdeda38230586375324b710ffc58a27ff121ef8
Arthur <X3neYTB@gmail.com>
2021-10-13 14:10:03 +0200

    add command template

---
+++ commands/command-template.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const { ErrorEmbed, SuccessEmbed, GetColor } = require('../../utils/functions');
+
+module.exports.run = async (client, msg, args, prefix) => {
+
+  // Command
+
+};
+
+module.exports.help = {
+  name: '',
+  aliases: [],
+  description: '',
+  usage: '',
+  category: '',
+};
